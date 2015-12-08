5290c37c636d7f7021df405efef6f3a57453fa8d
Nicolas <nicolasmatt.hetic@gmail.com>
2015-12-08 15:32:47 +0100

    suite inté slideshow

--- wp-content/themes/amillion/tpl-home.php
+++ wp-content/themes/amillion/tpl-home.php
@@ -9,64 +9,82 @@ get_header(); ?>
 
 <div class="slideshow">
 
-<div class='pushs'>
+	<div class='pushs'>
 
-	<div class='push push-library'>
-		<span>Order</span>
-		<p class="push-title">Music Library</p>
-		<p>Pre-cleared music<br>Curated for ads</p>
-		<a href="#" class="btn btn-push">Access</a>
+		<div class='push push-library'>
+			<span>Order</span>
+			<p class="push-title">Music Library</p>
+			<p>Pre-cleared music<br>Curated for ads</p>
+			<a href="#" class="btn btn-push">Access</a>
+		</div>
+
+		<div class='push push-production'>
+			<span>Order</span>
+			<p class="push-title">On demand production</p>
+			<p>Exclusive license<br>All rights included</p>
+			<a href="#" class="btn btn-push">Fill a brief</a>
+		</div>
+
+
 	</div>
 
-	<div class='push push-production'>
-		<span>Order</span>
-		<p class="push-title">On demand production</p>
-		<p>Exclusive license<br>All rights included</p>
-		<a href="#" class="btn btn-push">Fill a brief</a>
+	<div class="slideshow-frame">
+		<nav>
+			<a class="prev" href=""></a>
+			<a class="next" href=""></a>
+		</nav>
 	</div>
 
 
-</div>
 
-<div class="trame"></div>
+	<ul class="bxslider">
 
-<ul class="bxslider">
+		<?php
+			$type = 'projets';
+			$args=array(
+			  'post_type' => $type,
+			  'post_status' => 'publish',
+			  'posts_per_page' => -1,
+			  'caller_get_posts'=> 1);
 
-	<?php
-		$type = 'projet';
-		$args=array(
-		  'post_type' => $type,
-		  'post_status' => 'publish',
-		  'posts_per_page' => -1,
-		  'caller_get_posts'=> 1);
+			$my_query = null;
+			$my_query = new WP_Query($args);
+			if( $my_query->have_posts() ) {
+			  while ($my_query->have_posts()) : $my_query->the_post(); 
 
-		$my_query = null;
-		$my_query = new WP_Query($args);
-		if( $my_query->have_posts() ) {
-		  while ($my_query->have_posts()) : $my_query->the_post(); 
+			  ?>
 
-		  ?>
+				<li style="background: url(<?php echo get_field('image_slideshow')['sizes']['large']; ?>) no-repeat center; background-size: cover;">
+			  		<div class="content">
+			  			<h3><?php the_title(); ?></h3>
+				  		<p class="brand"><?php the_tags('', ', ', ''); ?></p>
+				  		<p class="type"><?php the_category( ', ' ); ?></p>
+				  	</div>
+			  	</li>
+			    
+			    <?php
+			  endwhile;
+			}
+			wp_reset_query();
+		?>
 
-			<li style="background: url(<?php echo get_field('image_slideshow')['sizes']['large']; ?>) no-repeat center; background-size: cover;">
-		  		<div class="content">
-		  			<h3><?php the_title(); ?></h3>
-			  		<p class="brand"><?php the_tags('', ', ', ''); ?></p>
-			  		<p class="type"><?php the_category( ', ' ); ?></p>
-			  	</div>
-		  	</li>
-		    
-		    <?php
-		  endwhile;
-		}
-		wp_reset_query();
-	?>
-
-</ul>
-
+	</ul>
 
 </div>
 
 <h2 class='title-nav'>Projets</h2>
+
+<div class="bloc bloc-projets">
+
+</div>
+
+<div class="bloc bloc-services">
+
+</div>
+
+<div class="bloc bloc-contact">
+
+</div>
 
 <h2 class='title-nav'>Services</h2>
 
